9634f629a09d7eea3d2d57cbeca7fb954af16179
GNANESWARAN Roshan <o22105014@info21-07.iut45.univ-orleans.fr>
2023-03-14 15:04:17 +0100

    ajt des fichiers de base

---
+++ requete.sql
@@ -0,0 +1,6 @@
+-- trouver un objet volé
+
+-- L’objet a été mis en vente en début de mois (avant le 15).
+-- Il a été mis en vente à une somme inférieure à 500€.
+-- Il a été vendu à plus de 10 fois le prix de vente initial
+
